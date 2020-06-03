93fdab6727028874558631a8c22262c2ed9fb4b7
Sonia Appasamy <sma246@cornell.edu>
2020-06-03 00:02:59 -0400

    phone groups

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,12 @@ const App: React.FC = () => {
     <div className="App" >
       <header className="App-header">
         <p>
-          Click below to send an email to your congressperson.
+          Click the links below to join the groups.
         </p>
-        <a className="App-link" href={`mailto:${SEND_TO}?subject=${SUBJECT}&body=${BODY}`}>Email Us</a>
+        <a className="App-link" href={`sms:55156&body=FLOYD`}>FLOYD</a>
+        <a className="App-link" href={`sms:668366&body=JUSTICE`}>JUSTICE</a>
+        <a className="App-link" href={`sms:55165&body=ENOUGH`}>ENOUGH</a>
+        {/* <a className="App-link" href={`mailto:${SEND_TO}?subject=${SUBJECT}&body=${BODY}`}>Email Us</a> */}
       </header>
     </div >
   );
