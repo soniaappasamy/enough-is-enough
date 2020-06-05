12a0af2d5a08c5e9f6a2136a6d9bbe4a3134438e
Sonia Appasamy <sma246@cornell.edu>
2020-06-05 15:51:34 -0400

    secure reroute

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import HttpsRedirect from 'react-https-redirect';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -8,7 +11,7 @@ import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HttpsRedirect> <App /> </HttpsRedirect>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
