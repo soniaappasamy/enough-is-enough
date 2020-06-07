a1b2e35984b6856aabfa504c2e805eeb5fbc30b9
acolink <63812366+acolink@users.noreply.github.com>
2020-06-07 11:48:28 -0400

    Update templates.ts

--- src/emails/templates.ts
+++ src/emails/templates.ts
@@ -635,30 +635,30 @@ export const allEmails: IEmail[] = [
         subject: 'DEFUND Pittsburgh Bureau of Police',
         secondInputPlaceholder: 'PITTSBURGH NEIGHBORHOOD',
         sendTo: [
-            'theresa.kail-smith@pittsburghpa.gov', 
-			'brosha.tkacheva@pittsburghpa.gov', 
-			'district5@pittsburghpa.gov', 
-			'district7@pittsburghpa.gov', 
-			'erika.strassburger@pittsburghpa.gov', 
-			'emilie.yonan@pittsburghpa.gov', 
-			'reverend.burgess@pittsburghpa.gov', 
-			'daniel.wood@pittsburghpa.gov', 
-			'curt.conrad@pittsburghpa.gov', 
-			'blake.plavchak@pittsburghpa.gov', 
-			'Kim.Salinetro@pittsburghpa.gov', 
-			'sally.stadelman@pittsburghpa.gov',
+            	'theresa.kail-smith@pittsburghpa.gov', 
+		'brosha.tkacheva@pittsburghpa.gov', 
+		'district5@pittsburghpa.gov', 
+		'district7@pittsburghpa.gov', 
+		'erika.strassburger@pittsburghpa.gov', 
+		'emilie.yonan@pittsburghpa.gov', 
+		'reverend.burgess@pittsburghpa.gov', 
+		'daniel.wood@pittsburghpa.gov', 
+		'curt.conrad@pittsburghpa.gov', 
+		'blake.plavchak@pittsburghpa.gov', 
+		'Kim.Salinetro@pittsburghpa.gov', 
+		'sally.stadelman@pittsburghpa.gov',
         ],
         body: (name, secondInput) => `Dear Pittsburgh City Council Members,
-          	${NEW_LINE}${NEW_LINE}My name is ${name}, and I am a resident of ${secondInput}.
-	    	${NEW_LINE}${NEW_LINE}I am writing to demand the city stops increasing the Pittsburgh Police Budget and redirects funding into supporting community operations.
-			${NEW_LINE}${NEW_LINE}The Pittsburgh Operating Budget's total designated expenditures allocate 40.0% to Public Safety and only 10.5% to Public Works. This is unacceptable.
-			${NEW_LINE}${NEW_LINE}The 2020 budget for the Bureau of Police is $114,787,000. This is ~18.9% - nearly one fifth - of the overall City Operations Budget.
-			${NEW_LINE}${NEW_LINE}There is only $1,758,022 allocated to the Office of Equity. The Ethics Hearing Board budget went down by $6,974 from 2019-2020 while the Police Bureau budget increased by $10,241,674. What is the need for this increase?
-			${NEW_LINE}${NEW_LINE}The first 2020 goal listed in the Bureau overview is “Continue to increase the ‘boots on the ground’ officers in the field”. Is this what the increase will go toward? What is the reasoning for needing more police on the ground? Due to the overwhelming evidence that the police are targeting Black people in this city (and across the world), it is the correct move to divest from funding boots on the ground operations and invest in upholding policies that support the people.
-			${NEW_LINE}${NEW_LINE}The Neighborhood and Community Development fund was only allocated $7,502,080 for 2020. That is ~6.8% of the 2020 Capital Budget. This is not enough. I demand you take action to re-allocate money away from the Pittsburgh Police and back into community development that includes affordable housing and reparations for Black citizens. Both the state and the Pittsburgh Police owe the Black community a great debt.
-			${NEW_LINE}${NEW_LINE}It is clear that Pittsburgh Police funds are not being appropriately used to maintain safety and freedom on the streets of Pittsburgh. This is not only obvious by the City Budget, it is obvious by how Pittsburghers are treated by the police. Black Pittsburghers are disproportionately subject to this violence. Antwon Rose was shot dead in our city by a police officer and we will always remember. Elijah Brewer was shot dead in our city by a police officer and we will always remember.
-            ${NEW_LINE}${NEW_LINE}Sincerely,
-           	${NEW_LINE}${name}
+		${NEW_LINE}${NEW_LINE}My name is ${name}, and I am a resident of ${secondInput}.
+		${NEW_LINE}${NEW_LINE}I am writing to demand the city stops increasing the Pittsburgh Police Budget and redirects funding into supporting community operations.
+		${NEW_LINE}${NEW_LINE}The Pittsburgh Operating Budget's total designated expenditures allocate 40.0% to Public Safety and only 10.5% to Public Works. This is unacceptable.
+		${NEW_LINE}${NEW_LINE}The 2020 budget for the Bureau of Police is $114,787,000. This is ~18.9% - nearly one fifth - of the overall City Operations Budget.
+		${NEW_LINE}${NEW_LINE}There is only $1,758,022 allocated to the Office of Equity. The Ethics Hearing Board budget went down by $6,974 from 2019-2020 while the Police Bureau budget increased by $10,241,674. What is the need for this increase?
+		${NEW_LINE}${NEW_LINE}The first 2020 goal listed in the Bureau overview is “Continue to increase the ‘boots on the ground’ officers in the field”. Is this what the increase will go toward? What is the reasoning for needing more police on the ground? Due to the overwhelming evidence that the police are targeting Black people in this city (and across the world), it is the correct move to divest from funding boots on the ground operations and invest in upholding policies that support the people.
+		${NEW_LINE}${NEW_LINE}The Neighborhood and Community Development fund was only allocated $7,502,080 for 2020. That is ~6.8% of the 2020 Capital Budget. This is not enough. I demand you take action to re-allocate money away from the Pittsburgh Police and back into community development that includes affordable housing and reparations for Black citizens. Both the state and the Pittsburgh Police owe the Black community a great debt.
+		${NEW_LINE}${NEW_LINE}It is clear that Pittsburgh Police funds are not being appropriately used to maintain safety and freedom on the streets of Pittsburgh. This is not only obvious by the City Budget, it is obvious by how Pittsburghers are treated by the police. Black Pittsburghers are disproportionately subject to this violence. Antwon Rose was shot dead in our city by a police officer and we will always remember. Elijah Brewer was shot dead in our city by a police officer and we will always remember.
+		${NEW_LINE}${NEW_LINE}Sincerely,
+		${NEW_LINE}${name}
             `,
     },
     {
